e68e46b6b636dc2e15ededec0573a4d88a04c873
Avishaan <avishaan@codeHatcher.com>
2014-08-06 13:31:59 -0700

    updated password for mail transporter

--- mail/transporter.js
+++ mail/transporter.js
@@ -2,7 +2,7 @@ var nodemailer = require('nodemailer');
 var transporter = module.exports = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: 'quipicsreset@codehatcher.com', 
+    user: 'quipicsreset@codehatcher.com',
     pass: 'badmin123'
   }
 });
