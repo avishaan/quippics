4d0244cdbc1b09666549746035a29a72923eff19
Avishaan <avishaan@codeHatcher.com>
2014-08-08 14:26:41 -0700

    add Activity model into mongooser config file for quick testing

--- tests/mongooser.js
+++ tests/mongooser.js
@@ -1,3 +1,4 @@
 exports.connect = 'mongodb://localhost/quippics';
 exports.User = require('./../models/user.js');
 exports.Challenge = require('./../models/challenge.js');
+exports.Activity = require('./../models/activity.js');
