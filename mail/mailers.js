bebd4c225ba273ec73816c5da38989f6bd5325a0
Avishaan <avishaan@codeHatcher.com>
2014-09-22 16:18:49 -0700

    need no callback, we aren't concerned with the result

--- mail/mailers.js
+++ mail/mailers.js
@@ -5,20 +5,20 @@ var logger = require('../logger/logger.js');
   * @param options Info about the mail message to send
   * @param options.flaggedUser The name of the user being flagged.
   */
-exports.mailFlaggedSubmission = function(options, cb){
+exports.moderateSubmission = function(options){
   var text = 'User:' + options.flaggedUser + ' submission has been flagged';
   transporter.sendMail({
-    from: 'test@quipics.com',
-    to: options.email,
+    from: 'moderate@quipics.com',
+    to: 'sleepyfloydshaan@gmail.com',
     subject: 'Quipics Flagged Submission',
     text: text
   }, function(err){
     if (!err){
       logger.info('Email send regarding flagged submission');
-      return cb(null);
+      //return cb(null);
     } else {
       logger.error('Error: Could not send flagged submission email');
-      return cb({clientMsg: "Could not send flagged submission email"});
+      //return cb({clientMsg: "Could not send flagged submission email"});
     }
   });
 
