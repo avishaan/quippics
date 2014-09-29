f6fd6e4445b034d6275bfca97179b9b21910d945
Avishaan <avishaan@codeHatcher.com>
2014-09-28 22:33:56 -0700

    compare to correct email address

--- tests/specs/submissionFlagSpecs.js
+++ tests/specs/submissionFlagSpecs.js
@@ -252,7 +252,7 @@ exports.spec = function(domain, callback){
         flagger: user3.id
       }, function(err, submission){
         expect(mailers.moderateSubmission).toHaveBeenCalled();
-        expect(mailers.moderateSubmission.mostRecentCall.args[0].flaggedUser).toEqual(user4.id);
+        expect(mailers.moderateSubmission.mostRecentCall.args[0].flaggedUser).toEqual(user1.email);
         done();
       });
     });
