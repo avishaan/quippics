5510ac8f5ec10092ebee4d714b5e6553195f87e9
Avishaan <avishaan@codeHatcher.com>
2015-01-05 00:15:17 -0500

    check no password sent to front end #86

--- tests/specs/submissionSpecs.js
+++ tests/specs/submissionSpecs.js
@@ -238,6 +238,7 @@ exports.spec = function(domain, callback){
         expect(submissions[0].thumbnail).not.toBeDefined();
         expect(submissions[0]._id).toBeDefined();
         expect(submissions[0].owner.username).toBeDefined();
+        expect(submissions[0].owner.password).not.toBeDefined();
         expect(submissions[0].owner).toBeDefined();
         expect(submissions[0].challenge).toBeDefined();
         expect(submissions[0].comments).toBeDefined();
