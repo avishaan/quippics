58e2e5400c34029a11bc1689b834f96698f38cde
Avishaan <avishaan@codeHatcher.com>
2015-03-26 20:22:18 -0400

    test: already returning data, add test for gh#145 route: /v2/challenges/:cid/submissions.

--- tests/specs/submissionV2Specs.js
+++ tests/specs/submissionV2Specs.js
@@ -176,6 +176,9 @@ exports.spec = function(domain, callback){
         expect(submission).toBeDefined();
         //make sure the id in the response body was returned
         expect(submission._id).toBeDefined();
+        // gh #145
+        console.log(res.body);
+        expect(submission.comments).toBeDefined();
         //expect 200 response
         expect(res.status).toEqual(200);
         submission1._id = submission._id;
