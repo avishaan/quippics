c9ae037a41ae987b64f0b3b31586ab508cd01111
Avishaan <avishaan@codeHatcher.com>
2015-05-06 00:50:25 -0700

    test: fix test date

--- tests/specs/challengePersistV2Specs.js
+++ tests/specs/challengePersistV2Specs.js
@@ -156,7 +156,7 @@ exports.spec = function(domain, callback){
         tags: ['tag1', 'tag2', 'tag3'],
         owner: user1._id,
         privacy: 'private',
-        expiration: new Date(2015, 3, 14),
+        expiration: (new Date()).setMonth(new Date().getMonth()+1),
         invites: [user2._id, user3._id]
       };
       frisby
@@ -237,7 +237,6 @@ exports.spec = function(domain, callback){
         expect(challenges[0].title).toBeDefined();
         expect(challenges[0].unscored).toBeDefined();
         expect(challenges[0].numParticipants).toBeDefined();
-        expect(challenges[0].inviteStatus).toEqual('invited'); //right now everyone is only invited
         cb(null);
       })
       .toss();
